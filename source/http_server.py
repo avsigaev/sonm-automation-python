78c0d9b71d355f3d3005cf0d5bcb25946c5e2dcf
Aleksander Efimov <aleksander_efimov@sonm.io>
2018-10-26 17:36:56 +0300

    added basic authorization

--- source/http_server.py
+++ source/http_server.py
@@ -1,3 +1,4 @@
+import base64
 import logging
 import threading
 import time
@@ -18,7 +19,31 @@ class SonmHttpServer:
 class HTTPServerRequestHandler(BaseHTTPRequestHandler):
     http_logger = logging.getLogger('monitor_http')
 
+    KEY = ''
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_authhead(self):
+        self.send_response(401)
+        self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
     def do_GET(self):
+
+        if self.headers.get('Authorization') is None:
+            self.do_authhead()
+            self.wfile.write('no auth header received'.encode('utf-8'))
+        elif self.headers.get('Authorization') == 'Basic ' + self.KEY:
+            self.handle_get()
+        else:
+            self.do_authhead()
+            self.wfile.write('not authenticated'.encode('utf-8'))
+
+    def handle_get(self):
         try:
             # TODO simple auth, price, clean state force (for task) , stop all (for task)
             # Check the file extension required and
@@ -74,7 +99,10 @@ def run_http_server():
     if "http_server" in Config.base_config and "run" in Config.base_config["http_server"]:
         if not Config.base_config["http_server"]["run"]:
             return
-
+        if "password" in Config.base_config["http_server"] and "user" in Config.base_config["http_server"]:
+            passwd_ = "{}:{}".format(Config.base_config["http_server"]["user"],
+                                     Config.base_config["http_server"]["password"])
+            HTTPServerRequestHandler.KEY = base64.b64encode(passwd_.encode('utf-8')).decode('utf-8')
         logger.info('Starting HTTP server...')
         server = HTTPServer(('0.0.0.0', Config.base_config["http_server"]["port"]), HTTPServerRequestHandler)
         logger.info("Agent started on port: {}".format(Config.base_config["http_server"]["port"]))
